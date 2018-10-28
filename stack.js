de2af2b2abd3f7fd92428a0817cb32d138586cdf
denisijcu <denisijcu266@gmail.com>
2018-10-27 23:50:13 -0400

    first commit

---
+++ stack.js
@@ -0,0 +1,25 @@
+console.log('Running');
+var trays = new Array();
+
+/*
+trays.push('A');
+document.write(trays.join() + '<br />');
+trays.push('C');
+document.write(trays.join() + '<br />');
+trays.push('D');
+
+document.write(trays.join() + '<br />');
+
+trays.pop();
+document.write(trays.join() + '<br />');
+trays.pop();
+document.write(trays.join() + '<br />');
+trays.pop();
+document.write(trays.join() + '<br />');
+
+*/
+trays.unshift('D');
+document.write(trays.join() + '<br />');
+trays.unshift('E');
+document.write(trays.join() + '<br />');
+trays.unshift('F');
